3a01c6355ff4cc98dc816f5d2100e9412ff52e8d
Abhinav Raj <abhinavmuzhakom@gmail.com>
2025-07-26 18:22:04 +0530

    .

--- ATS_app/ATS_app/settings.py
+++ ATS_app/ATS_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qkhg&k1#3wlq0vk_#h3mnh!8=@qr+)i1(!jcw)@ap(hb3@wpyr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
